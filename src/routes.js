f3e5d34db763796ac23550bee517ddce22338a9d
ErnestJo <smartlabstech@yahoo.com>
2023-01-04 16:52:57 +0300

    Route client configured

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const PropertiesManagement = React.lazy(() =>
   import('./views/propertiesManagement/PropertiesManagement'),
 )
-const EmployesManagement = React.lazy(() => import('./views/userManagement/UserManagement'))
+const clientsManagement = React.lazy(() => import('./views/userManagement/ClientManagement'))
 const staffManagement = React.lazy(() => import('./views/staffManagement/StaffManagement'))
 const PropertiesRequst = React.lazy(() => import('./views/propertiesRequest/PropertiesRequest'))
 const Reports = React.lazy(() => import('./views/reports/Reports'))
@@ -18,7 +18,7 @@ const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/staffss', name: 'Staff Management', element: PropertiesManagement },
-  { path: '/employee-management', name: 'Employes Management', element: EmployesManagement },
+  { path: '/client', name: 'Clients Management', element: clientsManagement },
   { path: '/staff', name: 'Staff Management', element: staffManagement },
   { path: '/properties-request', name: 'Properties Requst', element: PropertiesRequst },
   { path: '/reports', name: 'Reports', element: Reports },
