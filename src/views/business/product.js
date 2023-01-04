1aaccb86db0e1faffa8a6de00a0bb4e491d23eb9
ErnestJo <smartlabstech@yahoo.com>
2023-01-05 00:25:01 +0300

    get service data

--- src/views/business/product.js
+++ src/views/business/product.js
@@ -25,10 +25,144 @@ import DataGrid, {
   Paging,
 } from 'devextreme-react/data-grid'
 
-const GetCategory_URL = '/api/BusinessManagement/GetAllCategory'
+const GetCategory_URL = '/api/BusinessManagement/GetAllProduct'
 
 const Product = () => {
-  return <div>product</div>
+  const [modalShow, setModalShow] = React.useState(false)
+  const [dataProduct, setDataProduct] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  const applyFilterTypes = [
+    {
+      key: 'auto',
+      name: 'Immediately',
+    },
+    {
+      key: 'onClick',
+      name: 'On Button Click',
+    },
+  ]
+
+  const onExporting = (e) => {
+    const workbook = new ExcelJS.Workbook()
+    const worksheet = workbook.addWorksheet('User sheet')
+
+    exportDataGrid({
+      component: e.component,
+      worksheet: worksheet,
+      autoFilterEnabled: true,
+    }).then(() => {
+      workbook.xlsx.writeBuffer().then((buffer) => {
+        saveAs(new Blob([buffer], { type: 'application/octet-stream' }), 'UserList.xlsx')
+      })
+    })
+    e.cancel = true
+  }
+
+  const [state, setState] = useState({
+    data: [],
+    showFilterRow: true,
+    showHeaderFilter: true,
+    currentFilter: applyFilterTypes[0].key,
+    startDate: new Date(),
+    toggleAddNewUser: false,
+    endDate: new Date(),
+  })
+  var dataGrid = null
+
+  useEffect(() => {
+    if (dataProduct == 0) {
+      GetDataTable()
+    }
+  }, [dataProduct])
+
+  const GetDataTable = () => {
+    setIsLoading(true)
+    axios({
+      method: 'GET',
+      url: GetCategory_URL,
+    })
+      .then((data) => {
+        console.log(data.data.data)
+        setDataProduct(data.data.data)
+        setIsLoading(false)
+      })
+      .catch((err) => {
+        console.log(err)
+        setIsLoading(false)
+      })
+  }
+  return (
+    <div>
+      {isLoading && <LoadingSpinner />}
+      <Card>
+        <Card.Header>
+          <Button className="btn btn-success btn-sm float-right" onClick={() => setModalShow(true)}>
+            <Icon.PersonCheck />
+            Add New Product
+          </Button>
+        </Card.Header>
+        <Card.Body>
+          <DataGrid
+            ref={(ref) => (dataGrid = ref)}
+            dataSource={dataProduct}
+            showRowLines={true}
+            showBorders={true}
+          >
+            <Column dataField="productId" alignment="center" caption="Product Id" />
+            <Column dataField="productName" alignment="center" caption="Product Name" />
+            <Column
+              dataField="productPrice"
+              alignment="center"
+              format="currency"
+              caption="Product Price"
+            />
+            <Column dataField="categoryName" alignment="center" caption="category Name" />
+            <Column
+              dataField="productId"
+              alignment="center"
+              caption="Action"
+              allowFiltering={false}
+              cellRender={(e) => {
+                return (
+                  <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <CButton>
+                      <Icon.Eye />
+                    </CButton>
+
+                    <CButton className="mx-1">
+                      <Icon.Trash />
+                    </CButton>
+                  </div>
+                )
+              }}
+            />
+            <ColumnChooser enabled={true} />
+            <Selection mode="multiple" />
+            <Export enabled={true} fileName="category Name" allowExportSelectedData={true} />
+            <Paging defaultPageSize={5} />
+            <FilterRow visible={state.showFilterRow} applyFilter={state.currentFilter} />
+            <HeaderFilter visible={state.showHeaderFilter} />
+            <Selection mode="single" />
+            <SearchPanel visible={true} width={240} placeholder="Search..." />
+            <Summary>
+              <TotalItem column="categoryId" summaryType="count" />
+            </Summary>
+          </DataGrid>
+        </Card.Body>
+      </Card>
+
+      <Modal
+        show={modalShow}
+        className={'modal-success'}
+        size="lg"
+        aria-labelledby="contained-modal"
+        centered
+      >
+        <AddClient onHide={() => setModalShow(false)} reloadPage={GetDataTable} />
+      </Modal>
+    </div>
+  )
 }
 
 export default Product
